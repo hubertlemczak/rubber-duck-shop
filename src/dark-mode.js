fd0e24c00ab11cd063b0019553c6dc62b438d31d
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-27 18:19:31 +0100

    dark mode feature added

---
+++ src/dark-mode.js
@@ -0,0 +1,20 @@
+const darkModeBtn = document.querySelector(".dark-mode-toggle-btn")
+let darkModeStorage = localStorage.getItem("DARKMODE")
+
+const darkModeEnabled = () => {
+    document.body.classList.add("dark-mode")
+    darkModeStorage = localStorage.setItem("DARKMODE", "enabled")
+}
+
+const darkModeDisabled = () => {
+    document.body.classList.remove("dark-mode")
+    darkModeStorage = localStorage.setItem("DARKMODE", "disabled")
+}
+
+darkModeBtn.addEventListener("click", () => {
+    darkModeStorage = localStorage.getItem("DARKMODE")
+    if (darkModeStorage === "disabled") darkModeEnabled()
+    else darkModeDisabled()
+})
+
+if (darkModeStorage === "enabled") darkModeEnabled()
--- src/data.js
+++ src/data.js
@@ -2,11 +2,11 @@ const data = [
     {
         name: "Skyrim Rubber Duck",
         src: "../img/skyrim-duck.jpg",
-        price: "£14.28"
+        price: "£14.28",
     },
     {
         name: "Action Man Rubber Duck",
         src: "../img/action-man-duck.jpg",
-        price: "£7.49"
-    }
+        price: "£7.49",
+    },
 ]
