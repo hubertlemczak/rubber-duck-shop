590f8f1465c05df29d8fcb42591faefa18cff120
John Xander <johnblox5@gmail.com>
2022-06-30 12:52:05 +0200

    sortByPrice function

--- src/data.js
+++ src/data.js
@@ -1,60 +1,72 @@
 const data = [
     {
+        id: "sky",
         name: "Skyrim Rubber Duck",
         src: "./img/skyrim-duck.jpg",
         price: "£14.28",
     },
     {
+        id: "act",
         name: "Action Man Rubber Duck",
         src: "./img/action-man-duck.jpg",
         price: "£7.49",
     },
     {
+        id: "liz",
         name: "Queen Elizabeth Rubber Duck",
         src: "./img/lizzie-duck.jpg",
         price: "£8.99",
     },
     {
+        id: "pir",
         name: "Pirate Rubber Duck",
         src: "./img/pirate-duck.jpg",
         price: "£8.50",
     },
     {
+        id: "gan",
         name: "Gandalf Rubber Duck",
         src: "./img/gandalf-duck.jpg",
         price: "£16.42",
     },
     {
+        id: "bat",
         name: "Batman Rubber Duck",
         src: "./img/batman-duck.jpg",
         price: "£9.99",
     },
     {
+        id: "spi",
         name: "Spider-Man Rubber Duck",
         src: "./img/spiderman-duck.jpg",
         price: "£6.99",
     },
     {
+        id: "gra",
         name: "Graduate Rubber Duck",
         src: "./img/graduate-duck.jpg",
         price: "£8.99",
     },
     {
+        id: "uni",
         name: "Union Jack Rubber Duck",
         src: "./img/uk-duck.jpg",
         price: "£9.76",
     },
     {
+        id: "vad",
         name: "Darth Vader Rubber Duck",
         src: "./img/vader-duck.jpg",
         price: "£13.99",
     },
     {
+        id: "pol",
         name: "London Policeman Rubber Duck",
         src: "./img/police-duck.jpg",
         price: "£8.50",
     },
     {
+        id: "sha",
         name: "Shakespeare Rubber Duck",
         src: "./img/shakespeare-duck.jpg",
         price: "£10.99",
--- src/products.js
+++ src/products.js
@@ -1,12 +1,34 @@
 const products = document.getElementById("products")
+const sortBtn = document.getElementById("sortBtn")
 
-data.map((product) => {
-    const card = document.createElement("div")
-    card.innerHTML = `
-        <div class="card">
-            <img src=${product.src} alt=${product.name} />
-            <h3>${product.name}</h3>
-            <p>${product.price}</p>
-    `
-    products.appendChild(card)
-})
+const renderProducts = (productList) => {
+    productList.map((product) => {
+        const card = document.createElement("div")
+        card.innerHTML = `
+            <div class="card">
+                <img src=${product.src} alt=${product.name} />
+                <h3>${product.name}</h3>
+                <p>${product.price}</p>
+        `
+        products.appendChild(card)
+    })
+}
+
+renderProducts(data)
+
+const sortByPrice = () => {
+    products.innerHTML = ""
+    const sortedByPrice = []
+
+    data.map(duck => [+duck.price.slice(1), duck.id])
+        .sort((a, b) => a[0] - b[0])
+        .map(priceDuck => {
+            data.map(dataDuck => {
+                if (dataDuck.id === priceDuck[1]) sortedByPrice.push(dataDuck)
+            })
+    })
+
+    renderProducts(sortedByPrice)
+}
+
+sortBtn.addEventListener("click", () => sortByPrice())
