1dd659bc32939a38edd394e6a915ac709f9bbce3
John Xander <johnblox5@gmail.com>
2022-06-30 13:31:38 +0200

    ratings

--- src/data.js
+++ src/data.js
@@ -4,71 +4,83 @@ const data = [
         name: "Skyrim Rubber Duck",
         src: "./img/skyrim-duck.jpg",
         price: "£14.28",
+        rating: 4.8
     },
     {
         id: "act",
         name: "Action Man Rubber Duck",
         src: "./img/action-man-duck.jpg",
         price: "£7.49",
+        rating: 3.6
     },
     {
         id: "liz",
         name: "Queen Elizabeth Rubber Duck",
         src: "./img/lizzie-duck.jpg",
         price: "£8.99",
+        rating: 4.5
     },
     {
         id: "pir",
         name: "Pirate Rubber Duck",
         src: "./img/pirate-duck.jpg",
         price: "£8.50",
+        rating: 4.5
     },
     {
         id: "gan",
         name: "Gandalf Rubber Duck",
         src: "./img/gandalf-duck.jpg",
         price: "£16.42",
+        rating: 4.7
     },
     {
         id: "bat",
         name: "Batman Rubber Duck",
         src: "./img/batman-duck.jpg",
         price: "£9.99",
+        rating: 3.7
     },
     {
         id: "spi",
         name: "Spider-Man Rubber Duck",
         src: "./img/spiderman-duck.jpg",
         price: "£6.99",
+        rating: 3.6
     },
     {
         id: "gra",
         name: "Graduate Rubber Duck",
         src: "./img/graduate-duck.jpg",
         price: "£8.99",
+        rating: 4.6
     },
     {
         id: "uni",
         name: "Union Jack Rubber Duck",
         src: "./img/uk-duck.jpg",
         price: "£9.76",
+        rating: 5
     },
     {
         id: "vad",
         name: "Darth Vader Rubber Duck",
         src: "./img/vader-duck.jpg",
         price: "£13.99",
+        rating: 5
     },
     {
         id: "pol",
         name: "London Policeman Rubber Duck",
         src: "./img/police-duck.jpg",
         price: "£8.50",
+        rating: 5
     },
     {
         id: "sha",
         name: "Shakespeare Rubber Duck",
         src: "./img/shakespeare-duck.jpg",
         price: "£10.99",
+        rating: 4.6
     },
 ]
